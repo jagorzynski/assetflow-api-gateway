f94fb7ebf6cb852fa2f717b247779258f0ace57d
Gorzynski Jakub <gorzynski.jakub@protonmail.com>
2025-01-25 19:01:55 +0100

    init commit

---
+++ src/main/java/com/sothrose/assetflow_api_gateway/AssetflowApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.sothrose.assetflow_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetflowApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssetflowApiGatewayApplication.class, args);
+	}
+
+}
